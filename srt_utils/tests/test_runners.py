f0d52384eb89dbd082842ceb4c946cdbd909c342
Maria Sharabayko <msharabayko@haivision.com>
2020-01-23 12:33:46 +0100

    Updated unit tests for object_runners.py module

--- srt_utils/tests/test_runners.py
+++ srt_utils/tests/test_runners.py
@@ -1,68 +1,52 @@
+""" Unit tests for object_runners.py module """
 import pytest
 
-import srt_utils.objects as objects
-import srt_utils.object_runners as object_runners
-import srt_utils.runners as runners
+from srt_utils.objects import SrtXtransmit, Tshark
+from srt_utils.object_runners import LocalRunner, RemoteRunner
+from srt_utils.runners import SimpleFactory
 
+
+SRT_XTRANSMIT_CONFIG = {
+    'type': 'rcv',
+    'path': '../srt-xtransmit/_build/bin/srt-xtransmit',
+    'port': '4200',
+}
 
 TSHARK_CONFIG = {
+    'path': 'tshark',
     'interface': 'en0',
     'port': 4200,
-    'filepath': './dump.pcapng',
-}
-
-SRT_TEST_MESSAGING_CONFIG = {
-    'path': '/Users/msharabayko/projects/srt/srt-maxlovic/_build',
-    'type': 'rcv',
-    'host': '',
-    'port': '4200',
-    'attrs_values': [
-        ('rcvbuf', '12058624'),
-        ('congestion', 'live'),
-        ('maxcon', '50'),
-    ],
-    'options_values': [
-        ('-msgsize', '1456'),
-        ('-reply', '0'),
-        ('-printmsg', '0'),
-    ],
-    'collect_stats': True,
-    'description': 'busy_waiting',
     'dirpath': '_results',
-} 
-
-SUBPROCESS_CONFIG = None
-
-SSH_SUBPROCESS_CONFIG = {
-    'username': 'msharabayko',
-    'host': '65.52.227.197',
 }
 
+LOCAL_RUNNER_CONFIG = {}
+
+REMOTE_RUNNER_CONFIG = {
+    'username': 'msharabayko',
+    'host': '137.116.228.51',
+}
 
 OBJECTS_CLASSES = [
-    ('tshark', TSHARK_CONFIG, objects.Tshark),
-    ('srt-test-messaging', SRT_TEST_MESSAGING_CONFIG, objects.SrtXtransmit),
+    ('srt-xtransmit', SRT_XTRANSMIT_CONFIG, SrtXtransmit),
+    ('tshark', TSHARK_CONFIG, Tshark),
 ]
 
-@pytest.mark.parametrize('obj_type, obj_config, class_name', OBJECTS_CLASSES)
-def test_factory_creates_right_object(obj_type, obj_config, class_name):
-    factory = runners.SimpleFactory()
+OBJRUNNERS_CLASSES = [
+    ('local-runner', LOCAL_RUNNER_CONFIG, LocalRunner),
+    ('remote-runner', REMOTE_RUNNER_CONFIG, RemoteRunner),
+]
+
+
+@pytest.mark.parametrize('obj_type, obj_config, classname', OBJECTS_CLASSES)
+def test_factory_creates_right_object(obj_type, obj_config, classname):
+    factory = SimpleFactory()
     obj = factory.create_object(obj_type, obj_config)
-    assert isinstance(obj, class_name)
+    assert isinstance(obj, classname)
 
 
-RUNNERS_CLASSES = [
-    ('subprocess', SUBPROCESS_CONFIG, object_runners.LocalRunner),
-    ('ssh-subprocess', SSH_SUBPROCESS_CONFIG, object_runners.RemoteRunner),
-]
-
-@pytest.mark.parametrize('runner_type, runner_config, class_name', RUNNERS_CLASSES)
-def test_factory_creates_right_runner(runner_type, runner_config, class_name):
-    factory = runners.SimpleFactory()
+@pytest.mark.parametrize('runner_type, runner_config, classname', OBJRUNNERS_CLASSES)
+def test_factory_creates_right_runner(runner_type, runner_config, classname):
+    factory = SimpleFactory()
     obj = factory.create_object('tshark', TSHARK_CONFIG)
     runner = factory.create_runner(obj, runner_type, runner_config)
-    assert isinstance(runner, class_name)
-
-
-# TODO: Tests for IObject.make_args()
-#       Fucntional tests: IRunner.start(), stop() with get_status() and ps -A
+    assert isinstance(runner, classname)
