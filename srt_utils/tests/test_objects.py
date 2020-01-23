25ecca9ea84fd75d62a50a0e547da1a9011070d0
Maria Sharabayko <msharabayko@haivision.com>
2020-01-23 12:07:04 +0100

    Unit tests for objects.py module

--- srt_utils/tests/test_objects.py
+++ srt_utils/tests/test_objects.py
@@ -1,51 +1,54 @@
+""" Unit tests for objects.py module """
 import pytest
 
-import srt_utils.objects as objects
 from srt_utils.runners import SimpleFactory
 
 
-# TSHARK_CONFIG = {
-#     'interface': 'en0',
-#     'port': 4200,
-#     'filepath': './dump.pcapng',
-# }
+CONFIG_0 = {
+    'type': 'rcv',
+    'path': '../srt-xtransmit/_build/bin/srt-xtransmit',
+    'port': '4200',
+}
+ARGS_0 = ['../srt-xtransmit/_build/bin/srt-xtransmit', 'receive', 'srt://:4200']
+ARGSSTR_0 = '../srt-xtransmit/_build/bin/srt-xtransmit receive "srt://:4200"'
+
 
 CONFIG_1 = {
     'type': 'rcv',
     'path': '../srt-xtransmit/_build/bin/srt-xtransmit',
     'port': '4200',
-}
-ARGS_1 = ['../srt-xtransmit/_build/bin/srt-xtransmit', 'receive', 'srt://:4200']
-
-CONFIG_2 = {
-    'type': 'rcv',
-    'path': '../srt-xtransmit/_build/bin/srt-xtransmit',
-    'port': '4200',
     'host': '',
 }
-ARGS_2 = ARGS_1
+ARGS_1 = ARGS_0
+ARGSSTR_1 = ARGSSTR_0
 
-CONFIG_3 = {
+
+CONFIG_2 = {
     'type': 'rcv',
     'path': '../srt-xtransmit/_build/bin/srt-xtransmit',
     'port': '4200',
     'statsdir': '_results',
     'statsfreq': '100'
 }
-ARGS_3 = [
+ARGS_2 = [
     '../srt-xtransmit/_build/bin/srt-xtransmit', 'receive', 'srt://:4200',
     '--statsfile', '_results/srt-xtransmit-stats-rcv.csv', '--statsfreq', '100'
 ]
+ARGSSTR_2 = '../srt-xtransmit/_build/bin/srt-xtransmit receive "srt://:4200" ' \
+    '--statsfile _results/srt-xtransmit-stats-rcv.csv --statsfreq 100'
 
-CONFIG_4 = {
+
+CONFIG_3 = {
     'type': 'snd',
     'path': '../srt-xtransmit/_build/bin/srt-xtransmit',
     'port': '4200',
     'host': '127.0.0.1',
 }
-ARGS_4 = ['../srt-xtransmit/_build/bin/srt-xtransmit', 'generate', 'srt://127.0.0.1:4200']
+ARGS_3 = ['../srt-xtransmit/_build/bin/srt-xtransmit', 'generate', 'srt://127.0.0.1:4200']
+ARGSSTR_3 = '../srt-xtransmit/_build/bin/srt-xtransmit generate "srt://127.0.0.1:4200"'
 
-CONFIG_5 = {
+
+CONFIG_4 = {
     'type': 'snd',
     'path': '../srt-xtransmit/_build/bin/srt-xtransmit',
     'port': '4200',
@@ -56,12 +59,15 @@ CONFIG_5 = {
         ('sndbuf', '1000000000'),
     ],
 }
-ARGS_5 = [
+ARGS_4 = [
     '../srt-xtransmit/_build/bin/srt-xtransmit', 'generate',
-    '"srt://127.0.0.1:4200?transtype=live&rcvbuf=1000000000&sndbuf=1000000000"'
+    'srt://127.0.0.1:4200?transtype=live&rcvbuf=1000000000&sndbuf=1000000000'
 ]
+ARGSSTR_4 = '../srt-xtransmit/_build/bin/srt-xtransmit generate ' \
+    '"srt://127.0.0.1:4200?transtype=live&rcvbuf=1000000000&sndbuf=1000000000"'
 
-CONFIG_6 = {
+
+CONFIG_5 = {
     'type': 'snd',
     'path': '../srt-xtransmit/_build/bin/srt-xtransmit',
     'port': '4200',
@@ -72,46 +78,56 @@ CONFIG_6 = {
         ('--duration', '10s'),
     ],
 }
-ARGS_6 = [
+ARGS_5 = [
     '../srt-xtransmit/_build/bin/srt-xtransmit', 'generate',
     'srt://127.0.0.1:4200', '--msgsize', '1316', '--sendrate', '15Mbps',
     '--duration', '10s'
 ]
+ARGSSTR_5 = '../srt-xtransmit/_build/bin/srt-xtransmit generate ' \
+    '"srt://127.0.0.1:4200" --msgsize 1316 --sendrate 15Mbps --duration 10s'
 
-# CONFIG_2 = {
-#     'path': '/Users/msharabayko/projects/srt/srt-maxlovic/_build',
-#     'type': 'rcv',
-#     'host': '',
-#     'port': '4200',
-#     'attrs_values': [
-#         ('rcvbuf', '12058624'),
-#         ('congestion', 'live'),
-#         ('maxcon', '50'),
-#     ],
-#     'options_values': [
-#         ('-msgsize', '1456'),
-#         ('-reply', '0'),
-#         ('-printmsg', '0'),
-#     ],
-#     'collect_stats': True,
-#     'description': 'busy_waiting',
-#     'dirpath': '_results',
-# }
-# ARGS_2 = ['']
 
+CONFIG_6 = {
+    'path': 'tshark',
+    'interface': 'en0',
+    'port': 4200,
+    'dirpath': '_results',
+}
+ARGS_6 = ['tshark', '-i', 'en0', '-f', 'udp port 4200', '-s', '1500', '-w', '_results/tshark-trace-file.pcapng']
+ARGSSTR_6 = 'tshark -i en0 -f "udp port 4200" -s 1500 -w _results/tshark-trace-file.pcapng'
 
 
 CONFIG_ARGS = [
+    ('srt-xtransmit', CONFIG_0, ARGS_0),
     ('srt-xtransmit', CONFIG_1, ARGS_1),
     ('srt-xtransmit', CONFIG_2, ARGS_2),
     ('srt-xtransmit', CONFIG_3, ARGS_3),
     ('srt-xtransmit', CONFIG_4, ARGS_4),
     ('srt-xtransmit', CONFIG_5, ARGS_5),
-    ('srt-xtransmit', CONFIG_6, ARGS_6),
+    ('tshark', CONFIG_6, ARGS_6),
 ]
+
+
+CONFIG_ARGSSTRS = [
+    ('srt-xtransmit', CONFIG_0, ARGSSTR_0),
+    ('srt-xtransmit', CONFIG_1, ARGSSTR_1),
+    ('srt-xtransmit', CONFIG_2, ARGSSTR_2),
+    ('srt-xtransmit', CONFIG_3, ARGSSTR_3),
+    ('srt-xtransmit', CONFIG_4, ARGSSTR_4),
+    ('srt-xtransmit', CONFIG_5, ARGSSTR_5),
+    ('tshark', CONFIG_6, ARGSSTR_6),
+]
+
 
 @pytest.mark.parametrize('type, config, args', CONFIG_ARGS)
 def test_make_args(type, config, args):
     factory = SimpleFactory()
     obj = factory.create_object(type, config)
     assert args == obj.make_args()
+
+
+@pytest.mark.parametrize('type, config, args_str', CONFIG_ARGSSTRS)
+def test_make_str(type, config, args_str):
+    factory = SimpleFactory()
+    obj = factory.create_object(type, config)
+    assert args_str == obj.make_str()
