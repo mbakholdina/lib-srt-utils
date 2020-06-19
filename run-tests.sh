6474e17cc0d20e8dd4b7591dfc5242c2a4ed5ebc
Maria Sharabayko <msharabayko@haivision.com>
2020-06-19 19:39:13 +0200

    Updated run-tests file with a loop for sendrate

--- run-tests.sh
+++ run-tests.sh
@@ -6,11 +6,11 @@
 #    TAKE=$1
 #fi
 
-
-for ((latency=10; latency <= 80; latency+=5))
+for sendrate in 5 10 20 50
   do
-    # echo $latency
-    venv/bin/python -m scripts.experiment_runner --resultsdir _send_buffer_16.06.20_rtt20_loss0_2mins/_rtt20_loss0_sendrate10_latency$latency  --latency $latency configs/send_buffer/rere-flip-flop.json
-    #venv/bin/python compute-loss.py _nak/_loss4_latency_$latency/msharabayko@10.129.10.165/1-srt-xtransmit-stats-rcv.csv _nak/_loss4_latency_$latency/local/2-srt-xtransmit-stats-snd.csv >> results_loss4.txt
+    for ((latency=20; latency <= 160; latency+=10))
+      do
+        venv/bin/python -m scripts.experiment_runner --resultsdir _send_buffer_19.06.20_rtt40_loss0_2mins/_rtt40_loss0_sendrate${sendrate}_latency$latency  --latency $latency --sendrate ${sendrate}Mbps configs/send_buffer/rere-flip-flop.json
+      done
   done
 
